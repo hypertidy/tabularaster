7788f5192970d9606deab3bb8a16c7d86bd759f2
Michael Sumner <mdsumner@gmail.com>
2016-07-22 14:40:22 +1000

    nested rasters

---
+++ R/nestedtabula.r
@@ -0,0 +1,45 @@
+## create a nested raster in a single row with all metadata and attribute table
+
+#' library(raster)
+#' library(maptools)
+#' data(wrld_simpl)
+#' r <- rasterize(wrld_simpl, raster(nrow = 256, ncol = 512))
+#' x <- rastertab(r)
+#' plot(x, 6)
+
+## buffer out to whole multiples of 2
+datatab <- function(x) {
+  tibble(val = values(x))
+}
+celltab <- function(x) {
+  tibble(cell = seq(ncell(x)))
+}
+
+library(tibble)
+rasterfrom1row <- function(x) {
+  do.call(raster, x[, c("xmn", "xmx", "ymn", "ymx", "nrow", "ncol", "crs")])
+}
+rastertab <- function(x, ...) UseMethod("rastertab")
+rastertab.RasterLayer <- function(x) {
+ x <- tibble(xmn = xmin(x), xmx = xmax(x), 
+                    ymn = ymin(x), ymx = ymax(x), 
+                    nrow = nrow(x), ncol = ncol(x), 
+                    crs = projection(x), 
+                    metadata = levels(x), 
+                    cells = list(dplyr::bind_cols(datatab(x), celltab(x))))
+
+
+ class(x) <- c("rastertab", class(x))
+
+ x
+}
+plot.rastertab <- function(x, y = 1, ...) {
+  stopifnot(length(y) == 1)
+  x <- x[1L, ]
+  actualraster <- rasterfrom1row(x)
+  acells <- x$cells[[1]]
+  actualraster[acells$cell] <- x$metadata[[1]][acells$val , y]
+  plot(actualraster, ...)
+}
+
+
