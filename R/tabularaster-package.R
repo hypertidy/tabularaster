2c199ca4560db4aca1e758303f655a8cb81f9dd5
Michael Sumner <mdsumner@gmail.com>
2020-04-10 19:55:20 +1000

    fix description

--- R/tabularaster-package.R
+++ R/tabularaster-package.R
@@ -1,13 +1,15 @@
-#' Tabular tools for raster, tidy tools for raster.
+#' Tabular tools for raster
+#' 
+#' Extract and index with raster tidy tools for raster.
+#' 
 #' 
 #' @name tabularaster
 #' @docType package
 #' @details Tabularaster includes these main functions.  
 #' \tabular{ll}{
-#'  \code{\link{as_tibble}}
 #'  \code{\link{as_tibble}} \tab convert raster data to data frame form, with control over output and form of dimension/coordinate columns \cr
 #'  \code{\link{cellnumbers}} \tab extract a data frame of query identifiers and cell,pixel index numbers \cr
-#'  \code{\link{extentFromCells}} \tab (now incorporated into raster itself) \cr
+#'  \code{\link{decimate}} \tab fast and loose resizing of a raster to coarser resolution \cr
 #'  \code{\link{index_extent}} \tab build an extent in row column form, as opposed to coordinate value form \cr
 #'  }
 NULL
